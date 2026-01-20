e91b017f1e7048de32c66910c3cbcf4218d59496
Trevor Cohen <tcohen511@gmail.com>
2026-01-19 21:13:00 -0500

    Add project version to cache version
    This ensures that the frontend cache is invalidated when the script
    changes.

--- backend/settings.py
+++ backend/settings.py
@@ -1,7 +1,9 @@
+import hashlib
+import tomllib
 from pathlib import Path
 from typing import Annotated, Any
 
-from pydantic import field_validator
+from pydantic import Field, field_validator
 from pydantic_settings import BaseSettings, NoDecode, SettingsConfigDict
 
 PROJECT_ROOT = str(Path(__file__).resolve().parents[1])
@@ -22,7 +24,28 @@ class Settings(BaseSettings):
     # (By default, pydantic-settings tries to JSON-decode list fields.)
     admin_user_ids: Annotated[list[str], NoDecode] = []
     log_level: str = "INFO"
-    cache_version: str = "1"
+    cache_version_override: str = Field(
+        default="1",
+        validation_alias="cache_version",
+    )
+
+    @property
+    def cache_version(self) -> str:
+        """
+        Computes a cache version using a hash that includes:
+        - The project version
+        - The env-configured cache version override (allows manual cache
+        invalidation)
+
+        This ensures that the frontend cache is invalidated the fetched script
+        changes.
+
+        Note that we do not include any additional config options that are
+        served to the client, since these are not cached.
+        """
+        values = [_get_project_version(), self.cache_version_override]
+        values_str = "|".join(values)
+        return hashlib.sha256(values_str.encode("utf-8")).hexdigest()
 
     @field_validator("admin_user_ids", mode="before")
     @classmethod
@@ -53,3 +76,16 @@ class Settings(BaseSettings):
 
 
 settings = Settings()  # type: ignore[call-arg]
+
+
+def _get_project_version() -> str:
+    """
+    Return the version of the application from the pyproject.toml file.
+    """
+    file_path = Path(f"{PROJECT_ROOT}/pyproject.toml")
+    data = tomllib.loads(file_path.read_text(encoding="utf-8"))
+    version = data.get("project", {}).get("version")
+    if version is None:
+        raise ValueError("Version not found in pyproject.toml")
+
+    return str(version)
