7af329f9a8d7d0f0ffdcd974a53be2b91c9c87cb
Trevor Cohen <tcohen511@gmail.com>
2026-01-19 19:48:05 -0500

    Separate routes into blueprints

---
+++ backend/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask
+
+from backend.db import init_db
+from backend.logger import logger
+from backend.settings import settings
+
+APP = Flask(__name__, root_path="/updoot")
+
+# pylint: disable=wrong-import-position
+from backend.routes.admin import ADMIN_BP
+from backend.routes.comments import COMMENTS_BP
+from backend.routes.recommendations import RECOMMENDATIONS_BP
+
+# pylint: enable=wrong-import-position
+
+APP.register_blueprint(ADMIN_BP)
+APP.register_blueprint(COMMENTS_BP)
+APP.register_blueprint(RECOMMENDATIONS_BP)
+
+
+if __name__ == "__main__":
+    if not os.path.exists(settings.db_path):
+        logger.info("Database not found, initializing")
+        init_db()
+    APP.run(host="0.0.0.0", port=8099)
